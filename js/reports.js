271dab555cabc969e8134d0b5f029fad6e38407c
Winifredo N. Ya-on Jr <wnyaonjr@gmail.com>
2016-11-12 16:20:48 +0800

    initial implementation of creating dynamic quarterly table and dynamic charts using hard-coded JSON data as sample input

--- js/reports.js
+++ js/reports.js
@@ -1,4 +1,4 @@
-var quarterReportJson = "[{\"year\":\"2016\",\"quarters\":[{\"date\":\"JAN-MAR2016\",\"numberOfCoaches\":0,\"numberOfVgLeaders\":454,\"numberOfVgMembers\":2184,\"numberOfVictoryGroups\":479},{\"date\":\"APR-JUN2016\",\"numberOfCoaches\":34,\"numberOfVgLeaders\":327,\"numberOfVgMembers\":1529,\"numberOfVictoryGroups\":326},{\"date\":\"JUL-SEP2016\",\"numberOfCoaches\":53,\"numberOfVgLeaders\":306,\"numberOfVgMembers\":1199,\"numberOfVictoryGroups\":292},{\"date\":\"OCT-DEC2016\",\"numberOfCoaches\":47,\"numberOfVgLeaders\":255,\"numberOfVgMembers\":1540,\"numberOfVictoryGroups\":341}]}]";
+var quarterReportJson = "[{\"year\":\"2016\",\"quarters\":[{\"date\":\"JAN-MAR2016\",\"numberOfCoaches\":0,\"numberOfVgLeaders\":454,\"numberOfVgMembers\":2184,\"numberOfVictoryGroups\":479},{\"date\":\"APR-JUN2016\",\"numberOfCoaches\":34,\"numberOfVgLeaders\":327,\"numberOfVgMembers\":1529,\"numberOfVictoryGroups\":326},{\"date\":\"JUL-SEP2016\",\"numberOfCoaches\":53,\"numberOfVgLeaders\":306,\"numberOfVgMembers\":1199,\"numberOfVictoryGroups\":292},{\"date\":\"OCT-DEC2016\",\"numberOfCoaches\":47,\"numberOfVgLeaders\":255,\"numberOfVgMembers\":1540,\"numberOfVictoryGroups\":341}]},{\"year\":\"2016\",\"quarters\":[{\"date\":\"JAN-MAR2016\",\"numberOfCoaches\":0,\"numberOfVgLeaders\":454,\"numberOfVgMembers\":2184,\"numberOfVictoryGroups\":479},{\"date\":\"APR-JUN2016\",\"numberOfCoaches\":34,\"numberOfVgLeaders\":327,\"numberOfVgMembers\":1529,\"numberOfVictoryGroups\":326},{\"date\":\"JUL-SEP2016\",\"numberOfCoaches\":53,\"numberOfVgLeaders\":306,\"numberOfVgMembers\":1199,\"numberOfVictoryGroups\":292},{\"date\":\"OCT-DEC2016\",\"numberOfCoaches\":47,\"numberOfVgLeaders\":255,\"numberOfVgMembers\":1540,\"numberOfVictoryGroups\":341}]},{\"year\":\"2016\",\"quarters\":[{\"date\":\"JAN-MAR2016\",\"numberOfCoaches\":0,\"numberOfVgLeaders\":454,\"numberOfVgMembers\":2184,\"numberOfVictoryGroups\":479},{\"date\":\"APR-JUN2016\",\"numberOfCoaches\":34,\"numberOfVgLeaders\":327,\"numberOfVgMembers\":1529,\"numberOfVictoryGroups\":326},{\"date\":\"JUL-SEP2016\",\"numberOfCoaches\":53,\"numberOfVgLeaders\":306,\"numberOfVgMembers\":1199,\"numberOfVictoryGroups\":292},{\"date\":\"OCT-DEC2016\",\"numberOfCoaches\":47,\"numberOfVgLeaders\":255,\"numberOfVgMembers\":1540,\"numberOfVictoryGroups\":341}]},{\"year\":\"2016\",\"quarters\":[{\"date\":\"JAN-MAR2016\",\"numberOfCoaches\":0,\"numberOfVgLeaders\":454,\"numberOfVgMembers\":2184,\"numberOfVictoryGroups\":479},{\"date\":\"APR-JUN2016\",\"numberOfCoaches\":34,\"numberOfVgLeaders\":327,\"numberOfVgMembers\":1529,\"numberOfVictoryGroups\":326},{\"date\":\"JUL-SEP2016\",\"numberOfCoaches\":53,\"numberOfVgLeaders\":306,\"numberOfVgMembers\":1199,\"numberOfVictoryGroups\":292},{\"date\":\"OCT-DEC2016\",\"numberOfCoaches\":47,\"numberOfVgLeaders\":255,\"numberOfVgMembers\":1540,\"numberOfVictoryGroups\":341}]}]";
 
 function initQuarterlyReport()
 {
@@ -11,12 +11,14 @@ function initQuarterlyReport()
     
     //create table and graph for each content
     for(var i=0; i<quarterJsonObj.length; i++)
-    {
+    {       
         
+        var graphContainer = $('<div></div>').addClass('quarterlyGraph').attr('id', 'graph'+i);
         
+        //chart initialization
         var options = {
             chart: {
-            renderTo: 'container',
+            renderTo: 'graph'+i,
             type: 'column'
             },
 
@@ -54,11 +56,20 @@ function initQuarterlyReport()
             series: []
         };
         
-        
+        //table initialization
+        var table = $('<table></table>').addClass('quarterlyReport').attr('id', 'report'+i);
+        table.append( '<tr>' );
+        table.append( '<th>Date</th>' );
+        table.append( '<th>Number of Leadership Group</th>' );
+        table.append( '<th>Number of Victory Group Leaders</th>' );
+        table.append( '<th>Number of Victory Group Members</th>' );
+        table.append( '<th>Number of Victory Groups</th>' );
+        table.append( '</tr>' );
         
         for(var j=0; j<quarterJsonObj[i].quarters.length; j++)
         {
             
+            //chart update
             var sampleObject = {
                 name: quarterJsonObj[i].quarters[j].date,
                 data: [quarterJsonObj[i].quarters[j].numberOfCoaches,
@@ -69,10 +80,23 @@ function initQuarterlyReport()
 
             options.series.push(sampleObject);
             
+            //table update
+            
+            table.append( '<tr>' );
+            table.append( '<td>'+quarterJsonObj[i].quarters[j].date+'</td>');
+            table.append( '<td>'+quarterJsonObj[i].quarters[j].numberOfCoaches+'</td>');
+            table.append( '<td>'+quarterJsonObj[i].quarters[j].numberOfVgLeaders+'</td>');
+            table.append( '<td>'+quarterJsonObj[i].quarters[j].numberOfVgMembers+'</td>');
+            table.append( '<td>'+quarterJsonObj[i].quarters[j].numberOfVictoryGroups+'</td>');
+            table.append( '</tr>' );
+            
+            
         }   
         
-        var chart = new Highcharts.Chart(options);
+        $("#reports").append(table);
+        $("#reports").append(graphContainer);
         
+        var chart = new Highcharts.Chart(options);
         //console.log(quarterJsonObj[i].date+", "+quarterJsonObj[i].numberOfCoaches+", "+quarterJsonObj[i].numberOfVgLeaders+", "+quarterJsonObj[i].numberOfVgMembers+", "+quarterJsonObj[i].numberOfVictoryGroups);
     }
     
